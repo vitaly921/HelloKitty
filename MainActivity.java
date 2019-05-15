04f7dd9889e3cfa757f5a5fcb06f96f57611cd3d
vitaly921 <49693884+vitaly921@users.noreply.github.com>
2019-05-15 10:26:05 +0400

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.admin.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private TextView mHelloTextView;
+    private EditText mNameEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mHelloTextView = findViewById(R.id.textView);
+        mNameEditText = findViewById(R.id.editText);
+    }
+
+
+    public void onClick(View view) {
+        if(mNameEditText.getText().length()==0) {
+            mHelloTextView.setText("Hello, Kitty!");
+        }
+        else{
+            mHelloTextView.setText("Привет, " + mNameEditText.getText());
+        }
+    }
+
+
+}
+
+
